3aedb59f4e4d3fa300898b9bb350e0cf1d8ce1df
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-10-05 02:28:59 +0200

    création des répository

--- src/main/java/fr/hotel/repositories/AdminRepository.java
+++ src/main/java/fr/hotel/repositories/AdminRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import fr.hotel.entities.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+	
+	 /* SELECT * FROM admin WHERE username = :username OR password = :password*/
+	public Admin findByUsernameOrPassword(String username, String password);
 }
