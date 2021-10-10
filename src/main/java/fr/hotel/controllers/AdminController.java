e971d92a3b2309eef0cf6232321360b688eae532
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-10-10 13:13:54 +0200

    Finalisation de la sécurité

--- src/main/java/fr/hotel/controllers/AdminController.java
+++ src/main/java/fr/hotel/controllers/AdminController.java
@@ -1,5 +1,21 @@
 package fr.hotel.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.hotel.services.AdminService;
+
+
+
 public class AdminController  {
 
+	
+	@Autowired
+	AdminService adminService;
+	@RequestMapping(value = "/login/{username}", method = RequestMethod.GET)
+	public AdminService getUserByUsername(@PathVariable("username") String username) {
+		return (AdminService) adminService.findByUsername(username);
+	}
 }
